1cad9dea140f299ece46a085b28bebefffa9a4a7
Elmar Abdulkarimov <49064106+elmar8287@users.noreply.github.com>
2022-11-30 18:19:27 +0000

    Finished the header section

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './Header.css';
+import { BsArrowLeft, BsArrowRight } from "react-icons/bs";
 
 function Header() {
   return (
-    <div>
+    <div className='header'>
       <h1 className='month-name'>October 2022</h1>
+      <BsArrowLeft className='arrows' />
+      <BsArrowRight className='arrows' />
+      <button className='header-buttons'>Today</button>
+      <button className='header-buttons'>Roma GMT +02:00</button>
     </div>
   );
 }
